f315e7f57b06ce53bff5c542261553b6dde3aa19
Walker Christie <walker781@yahoo.com>
2015-01-11 10:35:24 -0500

    Updated OI file with new comment

--- src/org/usfirst/frc4311/OI.java
+++ src/org/usfirst/frc4311/OI.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj.*;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
+    //// CREATING BUTTONS 
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
     // number it is.
